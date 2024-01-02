0dc6e3c407b159a20749b0a488bfa72c410dee3b
Adam Cieślewicz <acadamus199@wp.pl>
2024-01-02 19:21:33 +0100

    ikj

--- app/src/main/java/com/example/donutclicker/MainActivity.kt
+++ app/src/main/java/com/example/donutclicker/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.donutclicker.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
 
 private lateinit var binding: ActivityMainBinding
 
@@ -20,19 +21,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(ActivityMainBinding.inflate(layoutInflater).root)
 
         //Variables
-        var holder = 10
+        var holder = 11
+        var img = R.drawable.full_donut
         val counter = findViewById<TextView>(R.id.counter)
-        val btn = findViewById<Button>(R.id.button)
-        btn.setOnClickListener{
-            val nextPage = Intent(this, SecondActivity::class.java)
-            startActivity(nextPage)
-        }
+        var dImage = findViewById<ImageView>(R.id.donut_image)
 
-        findViewById<ImageView>(R.id.donut_image).setOnClickListener {
-            holder--
-            counter.text = holder.toString()
-            val nextPage = Intent(this, SecondActivity::class.java)
-            startActivity(nextPage)
+        dImage.setOnClickListener {
+            when(holder){
+                10 -> img = R.drawable.full_donut
+                9 -> img = R.drawable.bitten_donut
+                5 -> img = R.drawable.bitten_donut2
+                0 -> {img = R.drawable.full_donut
+                    holder = 11}
+            }
+            dImage.setImageResource(img)
+            counter.text = (--holder).toString()
+            /*val nextPage = Intent(this, SecondActivity::class.java)
+            startActivity(nextPage)*/
         }
     }
 }
