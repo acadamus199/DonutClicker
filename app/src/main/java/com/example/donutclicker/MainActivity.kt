6fd5ae0e8ec2f14d90fe9d39fb9271b1752b976a
Adam Cieślewicz <acadamus199@wp.pl>
2024-01-03 23:04:56 +0100

    c

--- app/src/main/java/com/example/donutclicker/MainActivity.kt
+++ app/src/main/java/com/example/donutclicker/MainActivity.kt
@@ -1,43 +1,99 @@
 package com.example.donutclicker
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.example.donutclicker.databinding.ActivityMainBinding
-import kotlinx.coroutines.delay
 
 private lateinit var binding: ActivityMainBinding
 
-
-
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(ActivityMainBinding.inflate(layoutInflater).root)
 
         //Variables
+        val bundle = intent.extras
+        var counting = intent.getIntExtra("money",0)
         var holder = 11
+        var multiplier = 1
         var img = R.drawable.full_donut
         val counter = findViewById<TextView>(R.id.counter)
-        var dImage = findViewById<ImageView>(R.id.donut_image)
+        val dImage = findViewById<ImageView>(R.id.donut_image)
+        val shop = findViewById<ImageView>(R.id.shop)
+        val sound = findViewById<ImageView>(R.id.sound)
+        var mp = MediaPlayer()
+        var bool = true
+        var music = true
+        var unlockAuto = intent.getBooleanExtra("auto",false)
+        if(unlockAuto){
+            multiplier = 2
+        }else{
+            multiplier = 1
+        }
+        counter.text = counting.toString()
 
+        //Biting Effect on Donut
+        var bite = MediaPlayer()
+        bite.setDataSource(this,(Uri.parse("android.resource://"+this.packageName+"/"+R.raw.bite_effect)))
+        bite.prepare()
+
+        //In game Music
+        fun AudioStart(){
+            mp.setDataSource(this,(Uri.parse("android.resource://"+this.packageName+"/"+R.raw.cafe_music)))
+            mp.prepare()
+            mp.isLooping = true
+            mp.start()
+        }
+        fun AudioStop(){
+            mp.stop()
+            mp.release()
+            mp = MediaPlayer()
+        }
+        sound.setOnClickListener{
+            if(bool){
+                AudioStop()
+                bool = false
+                sound.setImageResource(R.drawable.play)
+            }else{
+                AudioStart()
+                bool = true
+                sound.setImageResource(R.drawable.mute)
+            }
+        }
+
+        //Counting clicks
         dImage.setOnClickListener {
             when(holder){
                 10 -> img = R.drawable.full_donut
-                9 -> img = R.drawable.bitten_donut
-                5 -> img = R.drawable.bitten_donut2
-                0 -> {img = R.drawable.full_donut
-                    holder = 11}
+                9 -> {
+                    img = R.drawable.bitten_donut
+                    bite.start()
+                }
+                5 -> {
+                    img = R.drawable.bitten_donut2
+                    bite.start()
+                }
+                0 -> {
+                    img = R.drawable.full_donut
+                    holder = 11
+                    bite.start()
+                }
             }
             dImage.setImageResource(img)
-            counter.text = (--holder).toString()
-            /*val nextPage = Intent(this, SecondActivity::class.java)
-            startActivity(nextPage)*/
+            --holder
+            counting += multiplier
+            counter.text = counting.toString()
+        }
+
+        //Changing window
+        shop.setOnClickListener {
+            music = false
+            startActivity(Intent(this, SecondActivity::class.java).putExtra("money",counting))
         }
     }
 }
--- app/src/main/java/com/example/donutclicker/SecondActivity.kt
+++ app/src/main/java/com/example/donutclicker/SecondActivity.kt
@@ -1,25 +1,50 @@
 package com.example.donutclicker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.ImageDecoder
+import android.graphics.drawable.AnimatedImageDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        //Variables
-        var holder = 10
-        val counter = findViewById<TextView>(R.id.counter)
+        var money = intent.getIntExtra("money",0)
+        val imageView: ImageView = findViewById<ImageView>(R.id.GIF)
+
+        findViewById<TextView>(R.id.counter).text = money.toString()
+        Thread{
+            val source: ImageDecoder.Source = ImageDecoder.createSource(resources, R.drawable.giphy)
+            val drawable: Drawable = ImageDecoder.decodeDrawable(source)
+            imageView.setImageDrawable(drawable)
+            (drawable as? AnimatedImageDrawable)?.start()
+        }.start()
 
 
-        findViewById<ImageView>(R.id.donut_image).setOnClickListener {
-            holder--
-            counter.text = holder.toString()
+        imageView.setOnClickListener{
+            if(money == 100) {
+                money -= 100
+                imageView.isClickable = false
+                var bundle = Bundle()
+                bundle.putInt("money", money)
+                bundle.putBoolean("auto", true)
+                startActivity(Intent(this, MainActivity::class.java).putExtras(bundle))
+                Toast.makeText(this, "U've been blessed by multiplier", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "Bring me 100 money", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        findViewById<ImageView>(R.id.shop).setOnClickListener {
             finish()
         }
+
 
     }
 }
